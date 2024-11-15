42b8b2c0f0fa04089dadd805a545055878375517
PhillGD <phillgd@outlook.com>
2024-11-15 12:44:29 +0000

    frame stuff

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -20,6 +22,9 @@ def main():
         black = pygame.Color("Black")
         screen.fill(black)
         pygame.display.flip()
+
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
