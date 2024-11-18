46336e32d4fdc1f08395e201762d241a4f691eb7
PhillGD <phillgd@outlook.com>
2024-11-18 12:22:51 +0000

    player movement

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updateable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+
+    Player.containers = (updateable_group, drawable_group)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -19,10 +23,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        for updateable in updateable_group:
+            updateable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        for drawable in drawable_group:
+            drawable.draw(screen)
+        
         pygame.display.flip()
 
+        # limit framerate 60fps
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
